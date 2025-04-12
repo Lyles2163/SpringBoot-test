2bd7c240cf77a303fd39b71bb708411669b67a92
Lyles2163 <13631155285@163.com>
2025-04-12 14:06:11 +0800

    Initial commit

---
+++ src/main/java/com/itheima/tliasweb/mapper/EmpExprMapper.java
@@ -0,0 +1,7 @@
+package com.itheima.tliasweb.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface EmpExprMapper {
+}
---
+++ src/main/java/com/itheima/tliasweb/service/impl/EmpServiceImpl.java
@@ -0,0 +1,8 @@
+package com.itheima.tliasweb.service.impl;
+
+import com.itheima.tliasweb.service.EmpService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmpServiceImpl implements EmpService {
+}
